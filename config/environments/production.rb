a917599f55e6d7ebddc729f5f0734e0792bb1e11
Hampton Catlin <hcatlin@greed-2.local>
2009-01-07 15:49:46 -0500

    increase the log level for initial work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
   c[:reload_classes] = false
-  c[:log_level] = :error
+  c[:log_level] = :debug
   
   c[:log_file]  = Merb.root / "log" / "production.log"
   # or redirect logger using IO handle
