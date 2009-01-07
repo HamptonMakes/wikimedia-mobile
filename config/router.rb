8ed34af83426b7d1a73181693eb2b817546e7542
Hampton Catlin <hcatlin@greed-2.local>
2009-01-07 18:56:33 -0500

    using with to DRY up the router

--- config/router.rb
+++ config/router.rb
@@ -30,13 +30,15 @@ Merb::Router.prepare do
   device_formats do # Sets up the formats for the device that is accessing the route
     match("/").to(:controller => "articles", :action => "home")
   
-    match(/\/wiki\/File:(.*)/).to(:controller => "articles", :action => "file", :file => "[1]")
+    with(:controller => "articles") do
+      match(/\/wiki\/File:(.*)/).to(:controller => "articles", :action => "file", :file => "[1]")
     
-    match(/\/wiki\/::Home/).to(:controller => "articles", :action => "home")
-    match(/\/wiki\/::Random/).to(:controller => "articles", :action => "random")
-    match(/\/wiki[\/]?(.*)/).to(:controller => "articles", :action => "show", :title => "[1]")
+      match(/\/wiki\/::Home/).to(:controller => "articles", :action => "home")
+      match(/\/wiki\/::Random/).to(:controller => "articles", :action => "random")
+      match(/\/wiki[\/]?(.*)/).to(:controller => "articles", :action => "show", :title => "[1]")
 
-    match("/w/index.php").to(:controller => "articles", :action => "show")
+      match("/w/index.php").to(:controller => "articles", :action => "show")
+    end
   
     # Change this for your home page to be available at /
     # match('/').to(:controller => 'whatever', :action =>'index')
