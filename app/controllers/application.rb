d9f2ce262e214234899524c32be57d5c70b7ff33
Hampton Catlin <hcatlin@greed-2.local>
2009-01-12 18:05:29 -0500

    adding more TODO items

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,7 +1,11 @@
 # This is the place for general todo's
 # TODO: Build out the application.wml.haml files so that we have WML support
 # TODO: Finish the CSS selectors in /config/wikipedia.yaml
-# TODO: Add in translations for view strings in /config/wikipedia.yaml
+# TODO: Add es to /config/wikipedia.yaml
+# TODO: Complete ja in /config/wikipedia.yaml
+# TODO: Add pt to /config/wikipedia.yaml
+# TODO: Make a request to m.wikipedia.org (no sub-sub-domain) redirect to whatever their browser's request is asking for
+# TODO: Cache the home page for a *particular* language for a day. Just do it locally in memory for the thread.
 
 class Application < Merb::Controller
   provides :webkit_native, :webkit, :wap
@@ -24,6 +28,7 @@ class Application < Merb::Controller
     @current_server ||= Server.new(request.language_code)
   end
   
+  # This is used right now in the alpha stage to log their user agent
   def print_ua
     if Merb.env != "test"
       Merb.logger.debug("User Agent: " + request.user_agent)
