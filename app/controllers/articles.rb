683c418a27654a6fa3699fc2afe6bcd1c5918e63
Colin Jones <trptcolin@gmail.com>
2009-01-15 23:46:15 -0500

    fix for nil current_name

--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -24,7 +24,7 @@ class Articles < Application
   
  private 
   def current_name
-    @name ||= (params[:search] || params[:title] || nil).gsub("_", " ")
+    @name ||= (params[:search] || params[:title] || "").gsub("_", " ")
   end
   
 end
