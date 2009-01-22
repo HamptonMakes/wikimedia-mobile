5993989a56be3f437c76da071481ee1c518d18c2
Hampton Catlin <hcatlin@greed-2.local>
2009-01-22 12:32:02 -0500

    some basic code style tweaks. im picky

--- lib/cache.rb
+++ lib/cache.rb
@@ -1,13 +1,15 @@
-# Simple cache for in memory caching of rendered pages
+# Simple cache for in memory caching of rendered def pages
+# TODO: Better document this library
+
 
 class Cache
   @@_cache= {}
   
   def self.cache(key, expires)
-    data= self.read(key)
+    data = self.read(key)
     unless data
-      data= yield # run action
-      self.write(key, data, :expires=>expires)
+      data = yield # run action
+      self.write(key, data, :expires => expires)
       # TODO: Find a better cache expiring strategy that considers when the original page on wikipedia gets updated
     end
     data
@@ -15,7 +17,7 @@ class Cache
   
   def self.read(key)
     return unless @@_cache[key]
-    if @@_cache[key][:expires].nil? || @@_cache[key][:expires]>Time.now
+    if @@_cache[key][:expires].nil? || (@@_cache[key][:expires] > Time.now)
       @@_cache[key][:data]
     else
       self.expire(key)
@@ -24,19 +26,20 @@ class Cache
   
   def self.write(key, data, options={})
     @@_cache[key] ||= {}
-    @@_cache[key][:expires]= options[:expires]
-    @@_cache[key][:data]= data
+    @@_cache[key][:expires] = options[:expires]
+    @@_cache[key][:data]    = data
   end
   
   def self.expire(key)
-    @@_cache[key]= nil
+    @@_cache[key] = nil
   end
 
   def self.swipe!
-    @@_cache={}
+    @@_cache = {}
   end
   
   def self.dump
+    # TODO: Uuuuugly.
     puts @@_cache.keys.map{|key| [key, @@_cache[key][:expires], @@_cache[key][:data][0..100]]}.inspect
   end
 end
