fe5f415b347e7ed3220b20cc16bc35621838131d
Hampton Catlin <hcatlin@greed.local>
2009-04-25 15:01:30 -0400

    strip before replacing spaces with underscore

--- app/models/wikipedia_resource.rb
+++ app/models/wikipedia_resource.rb
@@ -51,7 +51,7 @@ module Wikipedia
     # :api: public
     def escaped_title
       return "" if title.nil?
-      @escaped_title ||= title.gsub(" ", "_").split("|").first.strip
+      @escaped_title ||= title.split("|").first.strip.gsub(" ", "_")
     end
     
     def uri_escaped_title
