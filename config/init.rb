136607336368b8dfa596de99258079d89d7b30b9
Hampton Catlin <hcatlin@gmail.com>
2009-06-06 18:44:27 -0400

    trying LMC for cache

--- config/init.rb
+++ config/init.rb
@@ -23,7 +23,7 @@ Merb::Config.use do |c|
   c[:session_secret_key]  = 'ff0bc97fd0e7d3a1e9f62389270643c91d0991ec'  # required for cookie session store
 end
 
-Cache = Moneta::File.new(:path => (Merb.root + "/tmp/cache.lmc"))
+Cache = Moneta::LMC.new(:filename => (Merb.root + "/tmp/cache.lmc"))
 
 Merb::BootLoader.before_app_loads do
   Merb.push_path(:merb_extensions, Merb.root / "merb/extensions", "**/*.rb")  
