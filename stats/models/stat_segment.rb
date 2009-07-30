ebe2f3d8ebd348a35a2ab5b95ed82386befdd3e4
Hampton Catlin <hcatlin@gmail.com>
2009-07-30 19:13:51 -0400

    major re-write of stats server

--- stats/models/stat_segment.rb
+++ stats/models/stat_segment.rb
@@ -4,8 +4,7 @@ class StatSegment
   
   property :id, Serial
   property :time, Time, :indexed => true
-  property :time_length, String
-  property :time_string, String, :indexed => true
+  property :time_length, String, :indexed => true
   property :cache_hit_ratio, Float
   property :spider_cache_hit_ratio, Float
   property :hits, Integer
@@ -15,12 +14,19 @@ class StatSegment
   property :slowest_action_time, Float
   property :fastest_action_time, Float
   property :average_action_time, Float
-  property :median_action_time, Float
+  property :median_action_time, Float, :default => 0.0
   property :language_hits, Object, :lazy => false
   property :format_hits, Object, :lazy => false
-  property :language_format_hits, Object, :lazy => false
   property :cache_size, Integer
   property :load_average, Float
+  
+  def percent_of(attribute)
+    if attributes[attribute] == nil
+      return "N/A"
+    else
+      (attributes[attribute] / hits.to_f).round.to_s + "%"
+    end
+  end
   
   def requests_per_second
     hits / 60.0 / 60
@@ -34,6 +40,14 @@ class StatSegment
     language_hits["de"]
   end
   
+  def median_action_time_in_ms
+    (self.median_action_time || 0.0) * 1000
+  end
+  
+  def average_action_time_in_ms
+    (self.average_action_time || 0.0) * 1000
+  end
+  
   def other_lang_hits
     total = 0
     language_hits.each do |lang, hits|
@@ -42,5 +56,82 @@ class StatSegment
       end
     end
     total
+  end
+  
+  def self.merge_type_with_key(key)
+    case key
+    when :cache_hit_ratio, :spider_cache_hit_ratio, :average_action_time, :median_action_time, :cache_size, :load_average
+      :mean
+    when :hits, :redirects, :home_page_views, :error_count
+      :sum
+    when :format_hits, :language_hits
+      :hash_sum
+    else
+      nil
+    end 
+  end
+  
+  def self.merge(list_of_stats = [])
+    data = {}
+    list_of_stats.each do |stat|
+      data = stat.merge(data)
+    end
+    
+    data.keys.each do |key|
+      case StatSegment.merge_type_with_key(key)
+      when :mean
+        data[key] = data[key].mean
+      end
+    end
+    
+    data
+  end
+  
+  # Merges a specific stat with some dataset
+  def merge(data = {})
+    attributes.keys.each do |key|
+      case StatSegment.merge_type_with_key(key)
+      when :mean
+        data[key] ||= []
+        if attributes[key] != nil
+          data[key] << attributes[key]
+        end
+      when :sum
+        data[key] ||= 0
+        if attributes[key].is_a? Numeric
+          data[key] += attributes[key]
+        end
+      when :hash_sum
+        data[key] ||= {}
+        attributes[key].keys.each do |hash_key|
+          data[key][hash_key] ||= 0
+          data[key][hash_key] += attributes[key][hash_key]
+        end
+      end
+    end
+    data
+  end
+  
+  def self.all_time
+    StatSegment.merge(StatSegment.days)
+  end
+  
+  def self.days
+    days = ($first_day..(Date.today - 1)).to_a
+    days.collect do |date|
+      StatSegment.day(date)
+    end
+  end
+  
+  def self.day(date = Date.today)
+    day = StatSegment.first(:conditions => ["DATE(time) = ? and time_length = ?", date, "day"])
+    
+    if day.nil? || date == Date.today || date == (Date.today - 1)
+      day ||= StatSegment.new(:time_length => "day", :time => date)
+      day.attributes = merge(all(:conditions => ["DATE(time) = ? AND time_length = ?", date, "hour"]))
+      day.save
+    end
+    
+    day
   end
 end
