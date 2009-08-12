932cf26fffd10a43f00f6a41d2d8b46397068a8c
Hampton Catlin <hcatlin@gmail.com>
2009-08-12 10:45:29 -0400

    ok, ok, ok, so maybe i got it backwards

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,7 @@ module Merb
     
     def notice
       # Show 20% of the time
-      if request.language_code == "en" && (rand > 0.75)
+      if request.language_code == "en" && (rand > 0.25)
         notices = ["twitter", "tip_hidden_menu"]
         picked_notice = notices[rand(notices.size)]
         partial "notices/#{picked_notice}"
