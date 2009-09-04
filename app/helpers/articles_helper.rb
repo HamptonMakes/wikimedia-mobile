5bce5923f6fca0f31d45f1803b96c8060fbc5aaf
Hampton Catlin <hcatlin@gmail.com>
2009-09-04 15:40:44 -0400

    adding in a survey

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,8 +3,8 @@ module Merb
     
     def notice
       # Show 20% of the time
-      if request.language_code == "en" && (rand > 0.25)
-        notices = ["twitter", "tip_hidden_menu"]
+      if request.language_code == "en" #&& (rand > 0.05)
+        notices = ["feedback", "twitter", "tip_hidden_menu"]
         picked_notice = notices[rand(notices.size)]
         partial "notices/#{picked_notice}"
       end
