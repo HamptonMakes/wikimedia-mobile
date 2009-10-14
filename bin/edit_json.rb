88513ef6770728979e2ae38af582d6950166c908
Hampton Catlin <hcatlin@gmail.com>
2009-10-14 12:41:10 +0100

    removing old bin files..

---
+++ bin/edit_json.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+require File.join(File.dirname(__FILE__), "../2009-07-16/gems/environment")
+load File.join(File.dirname(__FILE__), "../2009-07-16/gems/gems/json_pure-1.1.9/bin/edit_json.rb")
