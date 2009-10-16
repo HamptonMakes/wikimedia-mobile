a5e948f2c35c27445b6955e96bfd940abde2a213
Hampton Catlin <hcatlin@gmail.com>
2009-10-16 15:12:50 +0100

    removing notices from native app

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,14 +1,12 @@
 module Merb
   module ArticlesHelper
-    
     def notice
       # Show 20% of the time
-      if request.language_code == "en" #&& (rand > 0.05)
+      if request.language_code == "en" && request.device.search_bar
         notices = ["feedback", "twitter", "tip_hidden_menu"]
         picked_notice = notices[rand(notices.size)]
         partial "notices/#{picked_notice}"
       end
     end
-
   end
 end # Merb
