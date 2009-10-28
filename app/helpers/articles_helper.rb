25c634e27a6b95345e9256d54f5ef8fe779dc40d
Hampton Catlin <hcatlin@gmail.com>
2009-10-28 10:53:41 +0000

    new donate header for US

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -3,7 +3,12 @@ module Merb
     def notice
       # Show 20% of the time
       if request.language_code == "en" && request.device.search_bar
-        notices = ["feedback", "twitter", "tip_hidden_menu"]
+        if request.country_code == "US"
+          notices = ["donate"]
+        else
+          notices = ["feedback", "twitter", "tip_hidden_menu"]
+        end
+        
         picked_notice = notices[rand(notices.size)]
         partial "notices/#{picked_notice}"
       end
