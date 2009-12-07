ba41c9be057e13cb3e640213577cdebb3949c25b
Hampton Catlin <hcatlin@gmail.com>
2009-12-07 22:48:37 +0000

    removing donation banner

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,12 +2,13 @@ module Merb
   module ArticlesHelper
     def notice
       # Show 20% of the time
-      if request.language_code == "en" && request.device.search_bar
-        if request.country_code == "US"
-          notices = ["donate"]
-        else
-          notices = ["feedback", "twitter", "tip_hidden_menu"]
-        end
+      if request.language_code == "en" && request.device.search_bar && (rand > 0.6)
+        #if request.country_code == "US"
+          #notices = ["donate"]
+          notices = ["twitter", "tip_hidden_menu"]
+        #else
+        #  notices = ["feedback", "twitter", "tip_hidden_menu"]
+        #end
         
         picked_notice = notices[rand(notices.size)]
         partial "notices/#{picked_notice}"
