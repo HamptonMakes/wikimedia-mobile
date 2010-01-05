a89d287f1f0fae9c4780921953d56b2a66d222e4
Hampton Catlin <hcatlin@gmail.com>
2010-01-05 17:07:29 +0000

    closing bug #22017

--- app/controllers/extensions/accessors.rb
+++ app/controllers/extensions/accessors.rb
@@ -1,25 +1,29 @@
 module ControllerExtensions
   module Accessors
     def supported_language?
-      Languages[request.language_code]
+      Languages[current_language]
     end
   
     def language_object
       @language_object ||= if supported_language?
-        Languages['en'].merge(Languages[request.language_code])
+        Languages['en'].merge(Languages[current_language])
       else
         Languages['en']
       end
     end
  
     def current_server
-      @current_server ||= Server.new(request.language_code)
+      @current_server ||= Server.new(current_language)
+    end
+    
+    def current_language
+      request.language_code
     end
   
     # This returns the setting object that is currently stored in /config/wikipedias.yml
     # this is used for wiki settings that aren't language
     def current_wiki
-      Wikipedia.settings[request.language_code] || {}
+      Wikipedia.settings[current_language] || {}
     end
     
     def home_page_path
