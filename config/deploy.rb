4b917c8e81be51b464b416d0ac3a9478f8c0ff93
Hampton Catlin <hcatlin@gmail.com>
2010-08-31 12:50:43 -0400

    fixing title encoding bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,13 +11,15 @@ set :use_sudo, false
 
 role :app, "mobile1.wikimedia.org", "mobile2.wikimedia.org", "mobile3.wikimedia.org"
 
+bin = "/var/lib/gems/1.9.1/bin"
+
 namespace :deploy do
 
   task :gems do
-    run "cd #{current_path} && /var/lib/gems/1.9.1/bin/bundle install"
+    run "cd #{current_path} && #{bin}/bundle install"
   end
 
-  task :after_update do
+  after :update do
     gems
   end
   
@@ -32,5 +34,11 @@ namespace :deploy do
   desc "Restart Application"
   task :restart, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
+  end
+end
+
+namespace :passenger do
+  task :memory_stats do
+    run "passenger-memory-stats"
   end
 end
