8c26573676d62e09130034556663c4f3d13fbef7
Hampton Catlin <hcatlin@gmail.com>
2010-12-23 13:27:55 +0000

    finally sorted out long-term issues with image file handling and namespaces

--- config/router.rb
+++ config/router.rb
@@ -45,8 +45,9 @@ Merb::Router.prepare do
         
         # File namespace
         if strings['file_namespace']
-          regex = Regexp.new("\/(#{strings['file_namespace']}):(.*)")
-          puts regex.inspect
+          namespace = URI.escape(strings['file_namespace'])
+          regex = Regexp.new("\/(#{namespace}):(.*)")
+          #puts regex.inspect
           varm.match(regex).to(:action => "file", :file => "[4]", :file_namespace => "[3]")
         end
       end
