c4330e43523bbf02c24140997d1c8f103348f964
Hampton Catlin <hcatlin@gmail.com>
2011-06-07 16:50:10 -0400

    Production logger is the error logger.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,11 +1,11 @@
 Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
-  c[:exception_details] = false
+  c[:exception_details] = true
   c[:reload_classes] = false
   c[:log_level] = :warn
   c[:default_cookie_domain] = ".wikipedia.org"
   
-  c[:log_file]  = Merb.root / "log" / "production.log"
+  c[:log_file]  = Merb.root / "log" / "error.log"
 
   # or redirect logger using IO handle
   #c[:log_stream] = STDOUT
