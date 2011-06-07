80cd807fcd85a82dbf14610b765aee0ee6a7fb2f
Hampton Catlin <hcatlin@gmail.com>
2011-06-07 17:07:44 -0400

    Add more error info

--- app/controllers/exceptions.rb
+++ app/controllers/exceptions.rb
@@ -4,6 +4,7 @@ class Exceptions < Application
 
     def standard_error
       #HoptoadNotifier.notify_hoptoad(request, session)
+      print_error
       render :format => :html
     end
   end
@@ -12,12 +13,18 @@ class Exceptions < Application
   def not_found
     #HoptoadNotifier.notify_hoptoad(request, session) if Merb.env == "production"
     #puts request.exceptions.first.inspect
+    print_error
     render :format => :html
   end
 
   # handle NotAcceptable exceptions (406)
   def not_acceptable
+    print_error
     render :format => :html
+  end
+
+  def print_error
+    Merb.logger.error("#{Time.now.to_s} --> #{request.path}")
   end
 
 end
